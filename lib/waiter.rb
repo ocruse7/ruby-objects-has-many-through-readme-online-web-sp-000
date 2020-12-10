75a402633655b9d70af21f536754ead9bddf4ae4
Owen Cruse <ocruse7@gmail.com>
2020-12-10 21:23:19 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+ 
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all.push(self)
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+   def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end 
+  
+  def meals
+    return Meal.all.select{|meal| meal.waiter == self}
+  end
 
+  def best_tipper
+    return meals.max{|a, b| a.tip <=> b.tip}.customer
+  end
 end
